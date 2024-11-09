d09636a7b0a390f1b88b79bbd2c5719a28c400de
Arpit Bhomia <arpitbhomia1204@gmail.com>
2024-11-09 22:22:44 +0530

    migration.sql Updated

--- prisma/migrations/20240714054958_init_schema/migration.sql
+++ prisma/migrations/20240714054958_init_schema/migration.sql
@@ -7,4 +7,13 @@ CREATE TABLE "User" (
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
+-- CreateTable2
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT NOT NULL,
 
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
