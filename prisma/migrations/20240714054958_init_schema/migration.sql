0d780fcdf12f8022d96a84f7d83bf6d558240a89
KartikeyaNainkhwal <kartikeyanainkhwal@gmail.com>
2024-11-10 00:10:38 +0530

    adding foreign key to connect User and post table in migration.sql

--- prisma/migrations/20240714054958_init_schema/migration.sql
+++ prisma/migrations/20240714054958_init_schema/migration.sql
@@ -17,3 +17,5 @@ CREATE TABLE "Post" (
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
