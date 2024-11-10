e88c42316324a252f0f9bdb50f406ffd742bf508
Arpit Bhomia <arpitbhomia1204@gmail.com>
2024-11-10 23:11:35 +0530

    Sign Up Router Added

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -13,3 +13,36 @@ export const userRouter=new Hono<{
          JWT_SECRET:string;
     }
 }>();
+
+userRouter.post('/signup',async(c)=>{
+    const body=await c.req.json();
+    const {success}=signupInput.safeParse(body);
+    if(!success){
+        c.status(411);
+        return c.json({
+            msg:"input is not correct"
+        })
+    }
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+    
+    try{
+      const user=await prisma.user.create({
+        data:{email:body.email,
+        name :body.name,
+        password :body.password
+      }
+    
+      })
+      const jwt =await sign({id:user.id},c.env.JWT_SECRET)
+      return c.text(jwt);
+    }
+    catch(e){
+      console.log(e);
+      c.status(411);
+      return c.text("invalid inputs");
+      
+    }
+  
+  })
