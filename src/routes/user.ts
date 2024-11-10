f62beaa9c6f387063a4ee26796550c941e5f99c4
[Ashish Ranjan] <[greatbrother864@gmail.com]>
2024-11-10 23:34:08 +0530

    adding the user sign in  function for user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -46,3 +46,42 @@ userRouter.post('/signup',async(c)=>{
     }
   
   })
+  
+    userRouter.post('/signin',async(c)=>{
+    const body=await c.req.json();
+    const {success}=signinInput.safeParse(body);
+    if(!success){
+        c.status(411);
+        return c.json({
+            msg:"input is not correct"
+        })
+    }
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+    
+    try{
+      const user=await prisma.user.findFirst({
+        where:{email:body.email,
+        password :body.password
+        }
+    
+      })
+      if(!user){
+          c.status(403);//unauth
+          return c.json({
+            "msg":"incorrect inputs"
+          })
+      }
+      const jwt =await sign({id:user.id},c.env.JWT_SECRET)
+      return c.text(jwt);
+    }
+    catch(e){
+      console.log(e);
+      c.status(411);
+      return c.text("invalid input");
+      
+    }
+  
+  
+  })
